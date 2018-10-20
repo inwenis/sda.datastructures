611c5fe77c52708f9a95d2a9ffdc1aa9e87ee95e
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-20 21:57:01 +0200

    add necessary classes

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,2 +1,53 @@
-public class LinkedList {
+public class LinkedList implements SdaList {
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int getFirst() {
+        return 0;
+    }
+
+    public int getLast() {
+        return 0;
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void addFirst(int element) {
+
+    }
+
+    public void addLast(int element) {
+
+    }
+
+    public void removeFirst() {
+
+    }
+
+    public void removeLast() {
+
+    }
+
+    public void setAt(int index, int element) {
+
+    }
+
+    public void addAt(int index, int element) {
+
+    }
+
+    public void removeAt(int index) {
+
+    }
 }
---
+++ src/main/java/SdaList.java
@@ -0,0 +1,81 @@
+public interface SdaList {
+
+    /**
+     * Zwraca true jeśli lista jest pusta.
+     */
+    boolean isEmpty();
+
+    /**
+     * Zwraca rozmiar listy (ilość elementów).
+     */
+    int size();
+
+    /**
+     * Zwraca pierwszy element listy.
+     *
+     * @throws NoSuchElementException jeśli lista jest pusta
+     */
+    int getFirst();
+
+    /**
+     * Zwraca ostatni element listy.
+     *
+     * @throws NoSuchElementException jeśli lista jest pusta
+     */
+    int getLast();
+
+    /**
+     * Pobiera element listy pod podanym indeksem.
+     *
+     * @throws IndexOutOfBoundsException jeśli indeks leży poza zakresem listy
+     */
+    int get(int index);
+
+    /**
+     * Usuwa wszystkie elementy z listy.
+     */
+    void clear();
+
+    /**
+     * Dodaje nowy element na początku listy.
+     */
+    void addFirst(int element);
+
+    /**
+     * Dodaje nowy element na końcu listy.
+     */
+    void addLast(int element);
+
+    /**
+     * Usuwa pierwszy element listy.
+     *
+     * @throws NoSuchElementException jeśli lista jest pusta
+     */
+    void removeFirst();
+
+    /**
+     * Usuwa ostatni element listy.
+     *
+     * @throws NoSuchElementException jeśli lista jest pusta
+     */
+    void removeLast();
+
+    /**
+     * Zamienia element pod podanym indeksem.
+     */
+    void setAt(int index, int element);
+
+    /**
+     * Dodaje nowy element pod wskazanym indeksem.
+     *
+     * @throws IndexOutOfBoundsException jeśli indeks leży poza zakresem listy
+     */
+    void addAt(int index, int element);
+
+    /**
+     * Usuwa element pod wskazanym indeksem.
+     *
+     * @throws IndexOutOfBoundsException jeśli indeks leży poza zakresem listy
+     */
+    void removeAt(int index);
+}
