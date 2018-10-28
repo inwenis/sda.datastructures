0db62439a61be1caf9904bb6654904911f9325ba
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-28 18:08:13 +0100

    handle push/pop for one number

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -1,7 +1,15 @@
 package stack;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Stack stack = new Stack();
+        stack.push(1);
+        Optional<Integer> pop = stack.pop();
+        System.out.println(pop.get());
+
+        stack.push(5);
+        System.out.println(stack.pop().get());
     }
 }
--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -1,4 +1,35 @@
 package stack;
 
+import java.util.Optional;
+
 public class Stack {
+    // push/pop/peek/isEmpty
+
+    Node head = null;
+
+    public void push(int i) {
+        if(head == null) {
+            head = new Node(i);
+        }
+    }
+
+    public Optional<Integer> pop() {
+        if(head == null) {
+            return Optional.empty();
+        } else {
+            Optional<Integer> num = Optional.of(head.num);
+            head = null;
+            return num;
+
+        }
+    }
+
+    class Node {
+        int num;
+        Node next;
+
+        Node(int num) {
+            this.num = num;
+        }
+    }
 }
