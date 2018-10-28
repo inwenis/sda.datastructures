de23c8a8f35e9bfc136dd3f7c6f24b8e9d2666e9
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-28 19:18:43 +0100

    add info strings

--- src/main/java/trees/Main.java
+++ src/main/java/trees/Main.java
@@ -16,29 +16,36 @@ public class Main {
                                 SdaTree.of(6,
                                         SdaTree.leaf(7),
                                         null)));
-
+        System.out.println("pre order");
         List<Integer> nodes = TreeTraverseExercises.traversePreOrder(exampleTree);
         for (Integer n : nodes) {
             System.out.print(n + " ");
         }
         System.out.println();
+        System.out.println();
 
+        System.out.println("pre order iterative");
         List<Integer> nodesPreOrderIterative = TreeTraverseExercises.traversePreOrderIterative(exampleTree);
         for (Integer n : nodesPreOrderIterative) {
             System.out.print(n + " ");
         }
         System.out.println();
+        System.out.println();
 
+        System.out.println("in order iterative");
         List<Integer> nodesInOrder = TreeTraverseExercises.traverseInOrder(exampleTree);
         for (Integer n : nodesInOrder) {
             System.out.print(n + " ");
         }
         System.out.println();
+        System.out.println();
 
+        System.out.println("post order iterative");
         List<Integer> nodesPostOrder = TreeTraverseExercises.traversePostOrder(exampleTree);
         for (Integer n : nodesPostOrder) {
             System.out.print(n + " ");
         }
+        System.out.println();
         System.out.println();
     }
 }
