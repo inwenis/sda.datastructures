5ae73d6a0c9a8dc247aa6938b073575e9743a19b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-30 08:19:18 +0100

    push/pop one value to heap

--- src/main/java/heap/Main.java
+++ src/main/java/heap/Main.java
@@ -2,6 +2,9 @@ package heap;
 
 public class Main {
     public static void main(String[] args) {
-
+        SdaHeap heap = new TreeHeap();
+        heap.push(4);
+        Integer pop = heap.pop();
+        System.out.println(pop);
     }
 }
--- src/main/java/heap/TreeHeap.java
+++ src/main/java/heap/TreeHeap.java
@@ -1,14 +1,25 @@
 package heap;
 
 public class TreeHeap implements SdaHeap {
+
+    Node root;
+
     @Override
     public void push(Integer element) {
-
+        if(root == null) {
+            root = new Node(element);
+        }
     }
 
     @Override
     public Integer pop() {
-        return null;
+        if(root == null) {
+            return null;
+        } else {
+            Node temp = this.root;
+            root = null;
+            return temp.value;
+        }
     }
 
     @Override
@@ -19,5 +30,15 @@ public class TreeHeap implements SdaHeap {
     @Override
     public Integer[] toArray() {
         return new Integer[0];
+    }
+
+    class Node {
+        Integer value;
+        Node left;
+        Node right;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
     }
 }
