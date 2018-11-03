ab2b38b2b067ee67a94db15b69bc0ad9fa57da2a
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-03 09:52:10 +0100

    add files for binary seach tree

---
+++ src/main/java/bst/BstTree.java
@@ -0,0 +1,25 @@
+package bst;
+
+import java.util.List;
+
+public class BstTree implements SdaBst {
+    @Override
+    public void insert(Integer element) {
+
+    }
+
+    @Override
+    public boolean contains(Integer element) {
+        return false;
+    }
+
+    @Override
+    public void delete(Integer element) {
+
+    }
+
+    @Override
+    public List<Integer> toList() {
+        return null;
+    }
+}
