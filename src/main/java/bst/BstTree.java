42b2b9e5a278cfba57a249a7d2c0988ba162b3ae
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-03 10:07:52 +0100

    implement insertions into bst

--- src/main/java/bst/BstTree.java
+++ src/main/java/bst/BstTree.java
@@ -1,11 +1,35 @@
 package bst;
 
+import heap.TreeHeap;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class BstTree implements SdaBst {
+
+    Node root;
+
     @Override
     public void insert(Integer element) {
-
+        Node newNode = new Node(element);
+        if(root == null) {
+            root = newNode;
+        } else {
+            Node traveler = root;
+            while (true) {
+                if (element <= traveler.value && traveler.left == null) {
+                    traveler.left = newNode;
+                    break;
+                } else if (element <= traveler.value && traveler.left != null) {
+                    traveler = traveler.left;
+                } else if (element > traveler.value && traveler.right == null) {
+                    traveler.right = newNode;
+                    break;
+                } else if (element > traveler.value && traveler.right != null) {
+                    traveler = traveler.right;
+                }
+            }
+        }
     }
 
     @Override
@@ -21,5 +45,41 @@ public class BstTree implements SdaBst {
     @Override
     public List<Integer> toList() {
         return null;
+    }
+
+    class Node {
+        Node left;
+        Node right;
+        Integer value;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+    }
+
+    public Integer[] toArray() {
+        Integer[] array = new Integer[100];
+        if(root != null) {
+            add(array, root, 0);
+        }
+
+        int lastNonEmptyIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] != null) {
+                lastNonEmptyIndex = i;
+            }
+        }
+
+        Integer[] truncatedArray = Arrays.copyOf(array, lastNonEmptyIndex + 1);
+
+        return truncatedArray;
+    }
+
+    private void add(Integer[] array, Node node, int index) {
+        if (node != null) {
+            array[index] = node.value;
+            add(array, node.left, index * 2 + 1);
+            add(array, node.right, index * 2 + 2);
+        }
     }
 }
--- src/main/java/bst/Main.java
+++ src/main/java/bst/Main.java
@@ -1,7 +1,21 @@
 package bst;
 
+import trees.TreePrinter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        SdaBst bst = new BstTree();
+
+        bst.insert(1);
+        bst.insert(2);
+        bst.insert(9);
+        bst.insert(8);
+        bst.insert(1);
+        bst.insert(5);
+        bst.insert(4);
+        bst.insert(7);
+
+        // cast to BstTree so we can access toArray() method which is not in the SdaBst interface
+        TreePrinter.coolPrint(((BstTree) bst).toArray());
     }
 }
